fa8eb68cc80fabdeafa8778c39fc88df5e3cbbb3
Sam <sgm@iastate.edu>
2023-10-15 15:47:47 -0500

    snowflake changes color based on graph temp

--- Midterm/dashboard.js
+++ Midterm/dashboard.js
@@ -1,49 +1,58 @@
 /* globals Chart:false */
+let temperatures = null;
+let snowflake = document.getElementById('snowflake-graph');
+let chartJson = {
+	type: 'line',
+	data: {
+		labels: [],
+		datasets: [{
+			data: [],
+			lineTension: 0,
+			backgroundColor: 'transparent',
+			borderColor: '#007bff',
+			borderWidth: 4,
+			pointBackgroundColor: '#007bff'
+		}]
+	},
+	options: {
+		plugins: {
+			legend: {
+				display: false
+			},
+			tooltip: {
+				boxPadding: 3
+			}
+		}
+	}
+}
 
-(() => {
-  'use strict'
+fetch('./dummyData.json')
+	.then(response => response.json())
+	.then(data => {
+		temperatures = data;
+		initializeChart();
+	});
 
-  // Graphs
-  const ctx = document.getElementById('myChart')
-  // eslint-disable-next-line no-unused-vars
-  const myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-      ],
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
-    },
-    options: {
-      plugins: {
-        legend: {
-          display: false
-        },
-        tooltip: {
-          boxPadding: 3
-        }
-      }
-    }
-  })
-})()
+function initializeChart() {
+	let averageTmp = 0;
+	temperatures?.data.forEach((tempObj) => {
+		averageTmp += tempObj.temperature_f;
+		let tempDate = new Date(tempObj.time);
+		chartJson.data.labels.push(tempDate.toLocaleDateString());
+		chartJson.data.datasets[0].data.push(tempObj.temperature_f);
+	});
+	averageTmp = averageTmp/temperatures?.data.length;
+	if(averageTmp > 80) {
+		snowflake.classList.add('red-snowflake');
+	} else if (averageTmp > 50) {
+		snowflake.classList.add('light-blue-snowflake')
+	} else {
+		snowflake.classList.add('blue-snowflake');
+	}
+
+	// Graphs
+	const ctx = document.getElementById('myChart')
+	// eslint-disable-next-line no-unused-vars
+
+	const myChart = new Chart(ctx, chartJson)
+}
