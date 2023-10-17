00ccf423653527811cd6e564ab1ffcfef5c4246a
Sam <sgm@iastate.edu>
2023-10-17 15:43:21 -0500

    added functionality to add product to cart

---
+++ Midterm/catalog.js
@@ -0,0 +1,15 @@
+let cartCountElement = document.getElementById('cart-count');
+let cartCount = 0;
+let addToCartButtons = document.getElementsByClassName('.add-to-cart');
+let cartContainer = document.getElementById('cart-container');
+
+cartContainer.addEventListener('click', function (event) {
+	if(event.target.classList.contains('add-to-cart')) {
+		const productId = event.target.getAttribute('data-product-id');
+		console.log(productId);
+		cartCount++;
+		cartCountElement.textContent = cartCount;
+		cartCountElement.style.fontWeight = 'bold';
+		//addToCart(productId);
+	}
+});
