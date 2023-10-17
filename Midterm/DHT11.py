b3ad6f893c37710c73841f62f5284184f48d2476
Elijah Brady <bradyeli09@gmail.com>
2023-10-17 14:11:03 -0500

    Remove gitignore and remove file writing

--- Midterm/DHT11.py
+++ Midterm/DHT11.py
@@ -1,15 +1,17 @@
 import time
 import board
 import adafruit_dht
-import os
 import json
 from datetime import datetime
 
 dhtDevice = adafruit_dht.DHT11(board.D4)
-api_url = f"https://api.github.com/repos/ElijahB09/secom319/contents/Midterm/dummyData.json"
+api_url = "https://api.github.com/repos/ElijahB09/secom319/contents/Midterm/data.json"
+access_token = "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"
+headers = {
+    "Authorization": f"Bearer {access_token}"
+}
 
 while True:
-    f = open(os.path.dirname(os.path.abspath(__file__)) + "/data.json", "w")
     json_objs = []
     x = 0
     while (x < 10):
@@ -27,6 +29,33 @@ while True:
         x = x + 1
     json_data = {"data": json_objs}
     json_string = json.dumps(json_data, indent=4)
-    f.write(json_string)
-    f.close()
+    response = requests.get(api_url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        
+        # Modify the JSON data as needed
+        current_content = json.loads(data["content"])
+        current_content["newField"] = "newData"
+        
+        # Encode the JSON content to base64
+        updated_content_base64 = json_string.encode("utf-8").b64encode()
+        
+        # Create a payload for the PATCH request
+        payload = {
+            "message": "Update JSON data",
+            "content": updated_content_base64.decode(),
+            "sha": data["sha"],  # SHA of the current content
+        }
+
+        # Make a PATCH request to update the file
+        response = requests.put(api_url, headers=headers, json=payload)
+
+        if response.status_code == 200:
+            print("JSON file updated successfully.")
+        else:
+            print(f"Failed to update the JSON file: {response.status_code} - {response.text}")
+
+    else:
+        print(f"Failed to fetch the JSON file: {response.status_code} - {response.text}")
     time.sleep(600.0)
