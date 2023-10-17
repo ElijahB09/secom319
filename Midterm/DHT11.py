a32f8ecb39b33596382fcb3c6176dc0c25140257
Elijah Brady <bradyeli09@gmail.com>
2023-10-17 14:08:26 -0500

    Change python to run constantly and update github json every ten minutes

--- Midterm/DHT11.py
+++ Midterm/DHT11.py
@@ -6,25 +6,27 @@ import json
 from datetime import datetime
 
 dhtDevice = adafruit_dht.DHT11(board.D4)
+api_url = f"https://api.github.com/repos/ElijahB09/secom319/contents/Midterm/dummyData.json"
 
-f = open(os.path.dirname(os.path.abspath(__file__)) + "/data.json", "w")
-json_objs = []
-x = 0
-
-while (x < 10):
-    try:
-        temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
-        humidity = dhtDevice.humidity
-        current_dateTime = datetime.now()
-        json_object = {"time": f"{current_dateTime}", "temperature_c": temperature_c, "temperature_f": temperature_f, "humidity": humidity}
-        json_objs.append(json_object)
-        print("{{ \"time\":\"{}\",\"temperature_c\":{:.2f},\"temperature_f\":{:.2f}, \"humidity\":{} }}".format(current_dateTime, temperature_c, temperature_f, humidity))
-    except RuntimeError as error:
-        print(error.args[0])
-    time.sleep(5.0)
-    x = x + 1
-json_data = {"data": json_objs}
-json_string = json.dumps(json_data, indent=4)
-f.write(json_string)
-f.close()
+while True:
+    f = open(os.path.dirname(os.path.abspath(__file__)) + "/data.json", "w")
+    json_objs = []
+    x = 0
+    while (x < 10):
+        try:
+            temperature_c = dhtDevice.temperature
+            temperature_f = temperature_c * (9 / 5) + 32
+            humidity = dhtDevice.humidity
+            current_dateTime = datetime.now()
+            json_object = {"time": f"{current_dateTime}", "temperature_c": temperature_c, "temperature_f": temperature_f, "humidity": humidity}
+            json_objs.append(json_object)
+            print("{{ \"time\":\"{}\",\"temperature_c\":{:.2f},\"temperature_f\":{:.2f}, \"humidity\":{} }}".format(current_dateTime, temperature_c, temperature_f, humidity))
+        except RuntimeError as error:
+            print(error.args[0])
+        time.sleep(5.0)
+        x = x + 1
+    json_data = {"data": json_objs}
+    json_string = json.dumps(json_data, indent=4)
+    f.write(json_string)
+    f.close()
+    time.sleep(600.0)
