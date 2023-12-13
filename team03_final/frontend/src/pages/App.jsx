9ad235e36ffe7187539af5eb1d2a044a023698fe
Sam <sgm@iastate.edu>
2023-12-13 13:08:49 -0600

    styled about us page

--- team03_final/frontend/src/pages/App.jsx
+++ team03_final/frontend/src/pages/App.jsx
@@ -4,7 +4,6 @@ import '../styling/dashboard.scss'
 import Header from '../components/Header';
 import Overview from './Overview';
 import {useState} from 'react';
-import ModalComp from '../components/ModalComp';
 
 const customStyles = {
 	content: {
@@ -35,19 +34,48 @@ function App() {
 
 	return (
 		<div className="App app-container" style={{background: '#336699'}}>
-			<Header searchVisible={searchVisible} addRoomOnClick={addRoomOnClick} showAboutUs={showAboutUs} showModal={showOverview}/>
-			<div className="container-fluid" style={{height: 'fit-content', paddingBottom:"50px", minHeight: '100vh'}}>
+			<Header searchVisible={searchVisible} addRoomOnClick={addRoomOnClick} showAboutUs={showAboutUs}
+					showModal={showOverview}/>
+			<div className="container-fluid" style={{height: 'fit-content', paddingBottom: "50px", minHeight: '100vh'}}>
 				<main className="col-md-8 col-lg-12 px-md-5">
 					{showOverview && <Overview
-					setSearchVisible={setSearchVisible}
-					roomModalVisible={modalVisible}
-					setRoomModalVisible={setModalVisible}/>}
+						setSearchVisible={setSearchVisible}
+						roomModalVisible={modalVisible}
+						setRoomModalVisible={setModalVisible}/>}
 					{aboutUs &&
-						<div>
-							<h1 style={{marginTop:"25px"}}>SE/COMS319 Construction of User Interfaces, Fall 2023</h1>
-							<h2>{(new Date()).toLocaleDateString()}</h2>
-							<h3>Sam McGrath: sgm@iastate.edu</h3>
-							<h3>Elijah Brady: ebrady@iastate.edu</h3>
+						<div className="about-us-container d-flex align-items-center justify-content-center">
+							<div style={{
+								background: '#6c757d',
+								borderRadius: '10px',
+								boxShadow: '0 5px 15px rgba(0, 0, 0, 0.15)',
+								padding: '20px',
+								margin: '10px',
+								width: '48%'
+							}}>
+								<h1 style={{marginTop: "25px"}}>SE/COMS319 Construction of User Interfaces, Fall
+									2023</h1>
+							</div>
+							<div style={{
+								background: '#6c757d',
+								borderRadius: '10px',
+								boxShadow: '0 5px 15px rgba(0, 0, 0, 0.15)',
+								padding: '20px',
+								margin: '10px',
+								width: '48%'
+							}}>
+								<h2>{(new Date()).toLocaleDateString()}</h2>
+							</div>
+							<div style={{
+								background: '#6c757d',
+								borderRadius: '10px',
+								boxShadow: '0 5px 15px rgba(0, 0, 0, 0.15)',
+								padding: '20px',
+								margin: '10px',
+								width: '48%'
+							}}>
+								<h3>Sam McGrath: sgm@iastate.edu</h3>
+								<h3>Elijah Brady: ebrady@iastate.edu</h3>
+							</div>
 						</div>
 					}
 				</main>
