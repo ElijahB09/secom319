ebd063a53573d7b4ca96fdbdf435adf1275e47d5
Sam <sgm@iastate.edu>
2023-12-13 15:44:54 -0600

    did stuff

--- team03_final/frontend/src/components/Header.jsx
+++ team03_final/frontend/src/components/Header.jsx
@@ -5,7 +5,6 @@ import {Button, Nav, Navbar} from 'react-bootstrap';
 
 function Header(props) {
 	return (
-
 		<Navbar id="nav-buttons-wrapp" bg="dark" variant="dark" expand="lg">
 
 			<Navbar.Brand id="title">M-C Brady Tech</Navbar.Brand>
@@ -14,15 +13,7 @@ function Header(props) {
 				<Nav.Link id="nav-dashboard" onClick={() => props.showAboutUs()}>About Us</Nav.Link>
 				{props.showModal && <Button style={{marginLeft:'75px'}} onClick={() => props.addRoomOnClick()}>Add Patient</Button>}
 			</Nav>
-			{/*{props.searchVisible && (*/}
-			{/*	<Form id="nav-form" style={{width: "65%"}}>*/}
-			{/*		<FormControl id="nav-search" type="text" placeholder="Search" className="mr-sm-2"*/}
-			{/*					 style={{width: "20%"}}/>*/}
-			{/*	</Form>*/}
-			{/*)}*/}
-
 		</Navbar>
-
 	);
 }
 
